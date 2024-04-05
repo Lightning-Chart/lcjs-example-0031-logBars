cc3c67f23dcfa9a4b490d4bd44157fb1502edef7
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:10:22 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const lcjs = require('@arction/lcjs')
 // Extract required parts from LightningChartJS.
 const { lightningChart, BarChartSorting, LegendBoxBuilders, Themes } = lcjs
 
-const barChart = lightningChart()
+const barChart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .BarChart({
         valueAxis: {
             type: 'logarithmic',
